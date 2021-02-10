0e898dc982d754b5e8d3c86963d1322a5d9a794c
Asyno <47446626+Asynoo@users.noreply.github.com>
2021-02-10 20:28:30 +0100

    It now enters new view when correct password

--- src/AttendanceProject/Gui/Controllers/LoginViewController.java
+++ src/AttendanceProject/Gui/Controllers/LoginViewController.java
@@ -1,18 +1,29 @@
 package AttendanceProject.Gui.Controllers;
 
+import AttendanceProject.Main;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class LoginViewController {
+public class LoginViewController  implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ;
+    }
 
 
     @FXML
@@ -28,19 +39,21 @@ public class LoginViewController {
     public void LoginAction(javafx.event.ActionEvent actionEvent) throws IOException {
         String user = username.getText();
         String pass = password.getText();
-        if(user.equals("admin")&&pass.equals("password"))
-        {
-            System.out.println("Poggers");
-        }else
+        if (user.equals("admin") && pass.equals("password")) {
+            Homepage();
+        } else
             System.out.println("Not Poggers");
     }
 
-    public void Homepage(Stage homepageView) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Gui/Views/HomepageView.fxml"));
-        homepageView.setTitle("Attendance Checker");
-        homepageView.setScene(new Scene(root));
-        homepageView.setResizable(false);
-        homepageView.show();
-    }
 
+    public void Homepage() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("Gui/Views/HomepageView.fxml"));
+        Stage MovieSettingsStage = new Stage();
+        MovieSettingsStage.setTitle("Attendance Checker");
+        MovieSettingsStage.setResizable(false);
+        MovieSettingsStage.show();
+
+
+    }
 }
