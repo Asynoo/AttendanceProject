68d5ec1530663f10e6fd620a7362c187ce52c285
Asyno <47446626+Asynoo@users.noreply.github.com>
2021-02-15 14:20:52 +0100

    Login Functional

--- src/AttendanceProject/Gui/Controllers/LoginViewController.java
+++ src/AttendanceProject/Gui/Controllers/LoginViewController.java
@@ -1,29 +1,15 @@
 package AttendanceProject.Gui.Controllers;
 
-import AttendanceProject.Main;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-
-public class LoginViewController  implements Initializable {
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        ;
-    }
+public class LoginViewController{
 
 
     @FXML
@@ -36,7 +22,7 @@ public class LoginViewController  implements Initializable {
     private JFXButton login;
 
     @FXML
-    public void LoginAction(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void LoginAction(javafx.event.ActionEvent actionEvent) throws Exception {
         String user = username.getText();
         String pass = password.getText();
         if (user.equals("admin") && pass.equals("password")) {
@@ -46,14 +32,14 @@ public class LoginViewController  implements Initializable {
     }
 
 
-    public void Homepage() {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("Gui/Views/HomepageView.fxml"));
-        Stage MovieSettingsStage = new Stage();
-        MovieSettingsStage.setTitle("Attendance Checker");
-        MovieSettingsStage.setResizable(false);
-        MovieSettingsStage.show();
 
-
+    public void Homepage() throws Exception {
+        Stage HomepageViewStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/AttendanceProject/Gui/Views/HomepageView.FXML"));
+        HomepageViewStage.setTitle("Attendance Checker");
+        HomepageViewStage.setScene(new Scene(root));
+        HomepageViewStage.setResizable(false);
+        HomepageViewStage.show();
     }
+
 }
