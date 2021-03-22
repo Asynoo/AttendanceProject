2e41ce0147cbee4cb072e805f2cfe9e039973282
Asyno <47446626+Asynoo@users.noreply.github.com>
2021-03-22 17:20:40 +0100

    Made New Branch for Comp Assignment 2.0

--- src/AttendanceProject/Gui/Controllers/TeacherHomepageViewController.java
+++ src/AttendanceProject/Gui/Controllers/TeacherHomepageViewController.java
@@ -9,7 +9,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
-
 import java.io.IOException;
 import java.util.Random;
 
