9f0be23eb68a0d08c2fc1a4f275492e5bfdc981d
Danylo <danyloboshagora@gmail.com>
2021-03-30 19:28:41 +0300

    Thanks Tomas. Listened to your advice on stack pane, now there is no problem changing between two scross panes.

--- src/AttendanceProject/Gui/Controllers/TeacherHomepageViewController.java
+++ src/AttendanceProject/Gui/Controllers/TeacherHomepageViewController.java
@@ -3,6 +3,7 @@ package AttendanceProject.Gui.Controllers;
 import AttendanceProject.Bll.AttendanceManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
@@ -11,40 +12,56 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 
-public class TeacherHomepageViewController {
+public class TeacherHomepageViewController implements Initializable {
     AttendanceManager attendanceManager;
-
     @FXML
-    private ScrollPane scrollPane;
+    private ScrollPane scrollStudentSummary;
+    @FXML
+    private ScrollPane scrollClassAttendance;
 
-    private Boolean studentsFilled = false;
+    private Boolean studentsClassVisible = false;
+    private Boolean studentsSummaryVisible = false;
     private final TilePane tilePaneClass = new TilePane();
     private final TilePane individualStudentTiles = new TilePane();
     private final int numberOfStudents = 19;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fillStudentsClass();
+        fillStudentsIndividually();
+        scrollClassAttendance.setVisible(false);
+        scrollStudentSummary.setVisible(false);
+    }
 
     public void showClassAttendance() {
-        if(studentsFilled){
-            tilePaneClass.setVisible(false);
-            studentsFilled = false;
-        }else{
-            fillStudentsClass();
+        if (!studentsClassVisible){
+            scrollClassAttendance.setVisible(true);
+            scrollStudentSummary.setVisible(false);
+            studentsClassVisible = true;
+        }else {
+            scrollClassAttendance.setVisible(false);
+            studentsClassVisible = false;
         }
-
     }
         
 
-    public void showStudentSummary() throws IOException {
-        fillStudentsIndividually();
+    public void showStudentSummary() {
+        if(!studentsSummaryVisible){
+            scrollStudentSummary.setVisible(true);
+            scrollClassAttendance.setVisible(false);
+            studentsSummaryVisible = true;
+        }else{
+            scrollStudentSummary.setVisible(false);
+            studentsSummaryVisible = false;
+        }
     }
 
         private void fillStudentsClass() {
             attendanceManager = new AttendanceManager();
-        if(tilePaneClass.isVisible()){
-            studentsFilled = true;
-
 
             tilePaneClass.setPrefTileHeight(110);
             tilePaneClass.setPrefTileWidth(110);
@@ -84,30 +101,27 @@ public class TeacherHomepageViewController {
                 }
 
             }
-            scrollPane.setContent(tilePaneClass);
-        }else{
-            tilePaneClass.setVisible(true);
-            studentsFilled = true;
-        }
-
-
+            scrollClassAttendance.setContent(tilePaneClass);
     }
 
-    private void fillStudentsIndividually() throws IOException {
+    private void fillStudentsIndividually() {
         individualStudentTiles.setPrefTileWidth(340);
         individualStudentTiles.setPrefColumns(1);
 
 
-        scrollPane.setContent(individualStudentTiles);
+        scrollStudentSummary.setContent(individualStudentTiles);
 
         for (int i = 0; i <numberOfStudents ; i++) {
             FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/AttendanceProject/Gui/Views/IndividualStudentSummary.fxml"));
-            HBox hb = loader.load();
+            HBox hb = null;
+            try {
+                hb = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             individualStudentTiles.getChildren().add(hb);
         }
-
-
-
     }
+
 
 }
